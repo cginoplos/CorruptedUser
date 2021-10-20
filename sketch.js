3f42a6bf4c27c25c6f0a74a54b1ac823e95f7918
cginoplos <80226192+cginoplos@users.noreply.github.com>
2021-10-20 17:57:58 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(1920, 1080);
+}
+//var gif_loadImg, gif_createImg;
+
+//function preload() {
+ // gif_loadImg = loadImage("giphy.gif");
+ // gif_createImg = createImg("giphy.gif");
+//}
